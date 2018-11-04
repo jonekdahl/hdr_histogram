34a4bfe48aff20e3db80333bc1114ae160cdb189
Jon Ekdahl <jon.ekdahl@gmail.com>
2018-11-04 18:07:42 +0100

    Remove failing dummy test

--- spec/hdr_histogram_spec.rb
+++ spec/hdr_histogram_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe HdrHistogram do
   it "has a version number" do
     expect(HdrHistogram::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
