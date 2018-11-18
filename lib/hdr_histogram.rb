d8ba75b18a7769410a7e22e50206d5f919eb46ea
Jon Ekdahl <jon.ekdahl@gmail.com>
2018-11-18 09:25:17 +0100

    Configure rubocop to assume al least Ruby 2.3

--- lib/hdr_histogram.rb
+++ lib/hdr_histogram.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'hdr_histogram/version'
 
 module HdrHistogram
--- lib/hdr_histogram/version.rb
+++ lib/hdr_histogram/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module HdrHistogram
   VERSION = '0.1.0'
 end
